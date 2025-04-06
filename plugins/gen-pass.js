8f74322f47698f03cebc824a7fbf28a74a94bfc6
Finjohns <kelvinonginjo08@gmail.com>
2025-04-07 02:18:35 +0300

    Update gen-pass.js

--- plugins/gen-pass.js
+++ plugins/gen-pass.js
@@ -56,7 +56,7 @@ cmd({
 
     // Send the message with the generated password
     await conn.sendMessage(from, {
-      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By JawadTechX*"
+      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By 𝙹𝙸𝙽𝚆𝙸𝙸𝙻𝚃𝙴𝙲𝙷𝚅*"
     }, {
       quoted: quoted
     });
