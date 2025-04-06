a8cb8da1a0aa39355f717b3786e22489eb6cfead
Finjohns <kelvinonginjo08@gmail.com>
2025-04-07 02:21:59 +0300

    Update fancy-text.js

--- plugins/fancy-text.js
+++ plugins/fancy-text.js
@@ -22,7 +22,7 @@ cmd({
     }
 
     const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
-    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *Powered by JAWADTECHX*`;
+    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *Powered by 𝙹𝙸𝙽𝚆𝙸𝙸𝙻𝚃𝙴𝙲𝙷𝚅*`;
 
     await conn.sendMessage(from, { text: resultText }, { quoted: m });
   } catch (error) {
