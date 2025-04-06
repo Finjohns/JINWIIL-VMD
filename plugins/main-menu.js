8cf061028ae86773dbea3b4c771182bd521055db
Finjohns <kelvinonginjo08@gmail.com>
2025-04-06 21:11:14 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -26,11 +26,11 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃★╰──────────────
 ╰━━━━━━━━━━━━━━━┈⊷
 ╭━━〔 *Download Menu* 〕━━┈⊷
-┃➬◈╭─────────────·๏
-┃➬◈┃• facebook
-┃➬◈┃• mediafire
-┃➬◈┃• tiktok
-┃➬◈┃• twitter
+>┃➬◈╭─────────────·๏
+>┃➬◈┃• facebook
+>┃➬◈┃• mediafire
+>┃➬◈┃• tiktok
+>┃➬◈┃• twitter
 ┃➬◈┃• Insta
 ┃➬◈┃• apk
 ┃➬◈┃• img
