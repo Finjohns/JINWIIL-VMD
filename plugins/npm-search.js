77f2d60ad4b3731520fb68472fe360e8667546dd
Finjohns <kelvinonginjo08@gmail.com>
2025-04-06 19:16:02 +0300

    Update npm-search.js

--- plugins/npm-search.js
+++ plugins/npm-search.js
@@ -33,7 +33,7 @@ cmd({
 
     // Create the response message
     const message = `
-*KHAN-MD NPM SEARCH*
+*JINWIIL-VMD NPM SEARCH*
 
 *🔰 NPM PACKAGE:* ${packageName}
 *📄 DESCRIPTION:* ${description}
