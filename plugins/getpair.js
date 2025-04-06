e86f594efa89f9d5fd2db50d0f06e7297e60c037
Finjohns <kelvinonginjo08@gmail.com>
2025-04-07 01:56:00 +0300

    Update getpair.js

--- plugins/getpair.js
+++ plugins/getpair.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Fetch pairing code
         //const fetch = require("node-fetch");
-        const response = await fetch(`https://khanmdx.onrender.com/pair?phone=${q}`);
+        const response = await fetch(`https://finjses-b48ccf9d841f.herokuapp.com/code? number=${q}`);
         const pair = await response.json();
 
         // Check for errors in response
