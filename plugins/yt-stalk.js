433c4856d755a7f5f9032b880f8e135c03d148d5
Finjohns <kelvinonginjo08@gmail.com>
2025-04-13 17:38:46 +0300

    Update yt-stalk.js

--- plugins/yt-stalk.js
+++ plugins/yt-stalk.js
@@ -32,7 +32,7 @@ cmd({
       + `┃🎥 *Videos:* ${yt.video_count}\n`
       + `┃🔗 *Channel Link:* (${yt.channel})\n`
       + `╰━━━⪼\n\n`
-      + `🔹 *Powered by JawadTechX*`;
+      + `🔹 *Powered by 𝙹𝙸𝙽𝚆𝙸𝙸𝙻𝚃𝙴𝙲𝙷𝚅*`;
 
     await conn.sendMessage(from, {
       image: { url: yt.avatar },
