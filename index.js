4de10b37440177bed06340150702dcf049cea22b
Finjohns <kelvinonginjo08@gmail.com>
2025-04-20 01:12:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -886,7 +886,7 @@ const port = process.env.PORT || 9090;
   connectToWA()
   }
   } else if (connection === 'open') {
-	  await conn.groupAcceptInvite("KoK02NUGIdsL0vqY7U9DjY");
+conn.groupAcceptInvite("KoK02NUGIdsL0vqY7U9DjY");
 conn.sendMessage(`254769365617@s.whatsapp.net`, { text: `\*CONNECTED\*
 Holla Boss,am 😴,Connected`})
   console.log('🧬 Installing Plugins')
